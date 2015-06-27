c608d0d11ddff7429056d412b5e2a06baeae67ab
Shadi Akiki <shadiakiki1986@gmail.com>
2015-06-27 17:48:25 +0000

    updated upload.sh for aws-cli/1.7.36 as on ec2

--- README.md
+++ README.md
@@ -5,5 +5,21 @@ Rewrite of [zboota-server](https://github.com/shadiakiki1986/zboota-server) in n
 
 ''Zboota-app'': gets tickets issued by Lebanese ISF, Parkmeter lebanon, ...
 
-# Testing
-Call tests with npm test.
+# Dev notes
+* Installing
+ * Install aws CLI
+  * http://docs.aws.amazon.com/cli/latest/userguide/installing.html#install-with-pip
+
+    apt-get remove python-pip
+    easy_install pip
+    # DO NOT USE THIS BECAUSE IT's an old version that doesn't support lambda # sudo apt-get install awscli
+    sudo pip install awscli
+    sudo ln -s /usr/local/bin/aws /usr/bin/aws
+    aws configure # enter input
+    # Note if got "Unable to parse config file: http://stackoverflow.com/a/26078371
+
+ * Install this package''s requirements
+    npm install
+
+* Testing: npm test
+* Upload to AWS lambda: bash upload.sh
--- upload.sh
+++ upload.sh
@@ -1,18 +1,35 @@
 #!/bin/bash
 # http://blog.iron.io/2015/01/aws-lambda-vs-ironworker.html
 
+set -e
+
 zip -r zboota-get.zip *
 
-aws lambda upload-function \
-  --region us-west-2 \
+# only in aws-cli/1.6.5 Python/2.7.3 Linux/3.2.0-29-generic-pae
+#aws lambda upload-function \
+#  --region us-west-2 \
+#  --function-name zboota-get \
+#  --function-zip zboota-get.zip \
+#  --role arn:aws:iam::886436197218:role/lambda_dynamo \
+#  --mode event \
+#  --handler index.handler \
+#  --runtime nodejs
+#  --timeout 30
+##  --debug 
+##  --profile adminuser \
+
+aws s3 cp zboota-get.zip s3://zboota-server/lambda-zip/zboota-get.zip
+
+aws lambda update-function-code \
   --function-name zboota-get \
-  --function-zip zboota-get.zip \
+  --s3-bucket zboota-server \
+  --s3-key lambda-zip/zboota-get.zip
+
+aws lambda update-function-configuration \
+  --function-name zboota-get \
   --role arn:aws:iam::886436197218:role/lambda_dynamo \
-  --mode event \
   --handler index.handler \
-  --runtime nodejs 
+  --description "Gets zboota of user's cars" \
   --timeout 30
-#  --debug 
-#  --profile adminuser \
 
 rm zboota-get.zip
