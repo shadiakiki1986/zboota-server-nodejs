bda0fe1524591ecbcbc4a85b09601cdf882a24ba
Shadi Akiki <shadiakiki1986@gmail.com>
2015-06-29 07:34:19 +0000

    moved MapArea to Validator+added tests + working on webdawlati

---
+++ test/ValidatorTest.js
@@ -0,0 +1,25 @@
+'use strict';
+var Validator = require('app/Validator');
+var should = require('should');
+
+describe('Validator tests', function() {
+
+	it('validArea', function() {
+	    Validator.validArea("B").should.eql(true);
+	    Validator.validArea("2").should.eql(false);
+	    Object.keys(Validator.mapIsf()).filter(function(x) { return Object.keys(Validator.mapPml()).indexOf(x)==-1; }).length.should.eql(0);
+	    Object.keys(Validator.mapPml()).filter(function(x) { return Object.keys(Validator.mapIsf()).indexOf(x)==-1; }).length.should.eql(0);
+	});
+
+	it('mapIsf', function() {
+	    Object.keys(Validator.mapIsf()).length.should.above(3);
+	    Validator.mapIsf()["B"].should.not.eql("B");
+	});
+
+	it('mapPml', function() {
+	    Object.keys(Validator.mapPml()).length.should.above(3);
+	    Validator.mapPml()["B"].should.not.eql("B");
+	});
+
+});
+
