da44e5458295b4cc5d2cb48d0665ea3bfde62190
Shadi Akiki <shadiakiki1986@gmail.com>
2015-07-13 06:31:10 +0000

    need to define every for Array in nodejs

---
+++ test/everyTest.js
@@ -0,0 +1,23 @@
+'use strict';
+require('app/every');
+
+describe('every tests', function() {
+
+  it('all true => true', function() {
+    [true,true,true].every().should.eql(true);
+  });
+
+  it('any false => false', function() {
+    [true,true,false,true].every().should.eql(false);
+  });
+
+  it('all false => false', function() {
+    [false,false,false].every().should.eql(false);
+  });
+
+  it('empty => true', function() {
+    [].every().should.eql(true);
+  });
+
+
+}); 
