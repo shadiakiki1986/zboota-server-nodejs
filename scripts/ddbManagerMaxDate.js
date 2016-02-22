ca585eff996d2d5e04ca9492cd2c0684bb05d858
Shadi Akiki <shadiakiki1986@gmail.com>
2016-02-22 15:42:40 +0200

    added scripts to force refresh data + minor webPML check that response is not null + check max data date

---
+++ scripts/ddbManagerMaxDate.js
@@ -0,0 +1,10 @@
+'use strict';
+var DdbManager = require('app/DdbManager');
+var context={
+    fail:function(err) { console.error("Shouldnt get here"); },
+    succeed: function(res) {
+      console.log("res",res);
+    }
+};
+var dm = new DdbManager();
+dm.registeredUsersDataMinDate(context,true);
